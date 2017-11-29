70f3c4d52da0e906c42c275700f8f8b9286b845b
Alana Owen <alanaowen@gmail.com>
2017-11-29 13:52:32 -0600

    added question array

---
+++ assets/javascript/app.js
@@ -0,0 +1,33 @@
+// Array for questions
+var questionsAsked = [
+  {  
+  	question: "Who are the two heroes that are going to fight Bowser?",
+  	options: ["Toad and Peach", "Spaghetti and Fetuccine", "Wario and Koopa", "Mario and Luigi"],
+  	answer: "Mario and Luigi",
+  	image: "../images/mario.jpg"
+  },
+  {
+  	question: "Which person is an opposing boxer in Mike Tyson's punchout?",
+  	options: ["Soda Popinksi", "Rocky", "Donkey Kong", "Bowser"],
+  	answer: "Soda Popinski",
+  	image: "../images/soda.jpg"
+  },
+  {
+  	question: "Who is the main character you control in The Legend of Zelda?",
+  	options: ["Ganon", "Zelda", "Link", "Mario"],
+  	answer: "Link",
+  	image: "../images/link.png"
+  },
+  {
+  	questions: "What is the cheat code on the controller for 30 lives in Contra?",
+  	options: [ "UP, UP, LEFT, RIGHT, LEFT, RIGHT, DOWN, DOWN, B, A, START", "UP, UP, DOWN, DOWN, LEFT, RIGHT, LEFT, RIGHT, B, A, START","DOWN, DOWN, RIGHT, RIGHT, LEFT, LEFT, UP, UP, A, B, START", "DOWN, UP, DOWN, UP, LEFT, LEFT, RIGHT, RIGHT, B, A, START"],
+  	answer: "UP, UP, DOWN, DOWN, LEFT, RIGHT, LEFT, RIGHT, B, A, START",
+  	image: "../images/contra.jpg"
+  },
+  {
+  	questions: "What was the best-selling game for the NES that was NOT packaged with most systems?",
+  	options: ["Super Mario Bros. 3", "Tetris", "Metroid", "Zelda"],
+  	answer: "Super Mario Bros. 3",
+  	image: "../images/supermario3.jpg"
+  },
+]
